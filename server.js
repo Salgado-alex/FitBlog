2b7e7fcfc1ab3d55dd8c81c9501b016f2bea7901
Alex <77710188+Salgado-alex@users.noreply.github.com>
2024-05-21 12:04:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -420,7 +420,7 @@ function generateAvatar(letter, width = 100, height = 100) {
   // TODO: Generate an avatar image with a letter
   // Steps:
   // 1. Choose a color scheme based on the letter
-  const color = '#0000FF';
+  const color = '#007bff';
   // 2. Create a canvas with the specified width and height
   const canvas = require('canvas').createCanvas(width, height);
   const ctx = canvas.getContext('2d');
